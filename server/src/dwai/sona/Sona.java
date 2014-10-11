68b87edbe0c7c0cd18d3871167515a812501af17
Joe <mooman219@gmail.com>
2014-10-11 16:43:33 -0400

    Initial commit of server

---
+++ server/src/dwai/sona/Sona.java
@@ -0,0 +1,17 @@
+package dwai.sona;
+
+/**
+ * Server
+ *
+ * @author Team DWAI
+ */
+public class Sona {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+
+}
